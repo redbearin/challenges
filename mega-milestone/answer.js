e89291d18821541a897c08e3559d245c9fa46e77
Juliann Talkington <perrigoj@bellsouth.net>
2021-11-07 20:21:26 -0600

    determine level of mega based on order of magnitude of number

---
+++ mega-milestone/answer.js
@@ -0,0 +1,20 @@
+const howMuchMega = num => {
+  // convert all numbers to positive values
+  num = Math.abs(num);
+  // no milestone
+  if (num < 100) 
+    return "not a mega milestone";
+  // a milestone
+  let str = '';
+  // add Mega to string for 100 and each order 
+  // of magnitude
+  while (num >=100) {
+    str += "MAGA "
+    num /= 10;
+  }
+  return str + "milestone";
+};
+
+const num = -9999.9;
+
+document.getElementById('ans').textContent = howMuchMega(num);
