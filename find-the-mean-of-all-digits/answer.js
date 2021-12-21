701600afcec884c752f9d5041010a2bcc4e23840
Juliann Talkington <perrigoj@bellsouth.net>
2021-12-20 21:54:27 -0600

    find avg of digits in num

---
+++ find-the-mean-of-all-digits/answer.js
@@ -0,0 +1,14 @@
+mean = num => {
+  const numStr = num.toString();
+  let sum = 0;
+  // go digit by digit
+  for (let i =0; i < numStr.length; i++) {
+    // add digit to sum
+    sum += +numStr[i];
+  }
+  return sum / numStr.length;
+}
+
+const num = 777;
+
+document.getElementById('ans').textContent = mean(num);
