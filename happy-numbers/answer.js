1b450d058de9d4e41d82caba24568dffc76cd14a
Juliann Talkington <perrigoj@bellsouth.net>
2022-02-19 20:56:56 -0600

    remove extra code

--- happy-numbers/answer.js
+++ happy-numbers/answer.js
@@ -1,16 +1,12 @@
 portionHappy = nums => {
   let happyCount = 0;
-  if (nums[0] === nums[1])
-    happyCount++;
-  if (nums[nums.length - 1] === nums[nums.length - 2])
-    happyCount++;
-  for (let i = 1; i < nums.length - 1; i++) {
+  for (let i = 0; i < nums.length; i++) {
     if (nums[i] === nums[i-1] || nums[i] === nums[i+1])
       happyCount++;
   }
   return happyCount/nums.length;
 }
 
-const nums = [1, 0, 0, 1, 1];
+const nums = [0, 1, 1, 0];
 
 document.getElementById('ans').textContent = portionHappy(nums);
