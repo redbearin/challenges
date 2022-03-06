139d3f66cc4f191b323f3df0f07ecf02a3edf1ab
Juliann Talkington <perrigoj@bellsouth.net>
2022-03-05 21:09:43 -0600

    error corrected

--- bird-names-to-four-letter-bird-names/answer.js
+++ bird-names-to-four-letter-bird-names/answer.js
@@ -5,25 +5,25 @@ birdCode = ([bird1, bird2])=> {
 
   if (bird1Arr.length === 4)
     abbrev.push(bird1Arr[0][0].toUpperCase() + bird1Arr[1][0].toUpperCase() + bird1Arr[2][0].toUpperCase() + bird1Arr[3][0].toUpperCase());
-  if (bird1Arr.length === 3)
+  else if (bird1Arr.length === 3)
     abbrev.push(bird1Arr[0][0].toUpperCase() + bird1Arr[1][0].toUpperCase() + bird1Arr[2][0].toUpperCase() + bird1Arr[2][1].toUpperCase());
-  if (bird1Arr.length === 2)
+  else if (bird1Arr.length === 2)
     abbrev.push(bird1Arr[0][0].toUpperCase() + bird1Arr[0][1].toUpperCase() + bird1Arr[1][0].toUpperCase() + bird1Arr[1][1].toUpperCase());
   else
     abbrev.push(bird1Arr[0][0].toUpperCase() + bird1Arr[0][1].toUpperCase() + bird1Arr[0][2].toUpperCase() + bird1Arr[0][3].toUpperCase());
 
   if (bird2Arr.length === 4)
     abbrev.push(bird2Arr[0][0].toUpperCase() + bird2Arr[1][0].toUpperCase() + bird2Arr[2][0].toUpperCase() + bird2Arr[3][0].toUpperCase());
-  if (bird2Arr.length === 3)
+  else if (bird2Arr.length === 3)
     abbrev.push(bird2Arr[0][0].toUpperCase() + bird2Arr[1][0].toUpperCase() + bird2Arr[2][0].toUpperCase() + bird2Arr[2][1].toUpperCase());
-  if (bird2Arr.length === 2)
+  else if (bird2Arr.length === 2)
     abbrev.push(bird2Arr[0][0].toUpperCase() + bird2Arr[0][1].toUpperCase() + bird2Arr[1][0].toUpperCase() + bird2Arr[1][1].toUpperCase());
   else
     abbrev.push(bird2Arr[0][0].toUpperCase() + bird2Arr[0][1].toUpperCase() + bird2Arr[0][2].toUpperCase() + bird2Arr[0][3].toUpperCase());
 
   return abbrev;
 }
-const bird1 = "Black-Capped Chickadee";
-const bird2 = "Common Tern";
+const bird1 = "Bobolink";
+const bird2 = "American White Pelican";
 
 document.getElementById('ans').textContent = birdCode([bird1, bird2]) 
