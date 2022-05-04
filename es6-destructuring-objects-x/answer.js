4f72f9053be11f7dc124d201a72fc6d7d5c994a9
Juliann Talkington <perrigoj@bellsouth.net>
2022-05-04 07:02:11 -0500

    for of loop to destructure objects

---
+++ es6-destructuring-objects-x/answer.js
@@ -0,0 +1,14 @@
+let names = [];
+
+let users = [
+  { name: "John", email: "john@example.com" },
+  { name: "Jason", email: "jason@example.com" },
+  { name: "Jeremy", email: "jeremy@example.com" },
+  { name: "Jacob", email: "jacob@example.com" }
+]
+
+for (let {name} of users) {
+  names.push(name);
+}
+
+document.getElementById('ans').textContent = JSON.stringify(names);
