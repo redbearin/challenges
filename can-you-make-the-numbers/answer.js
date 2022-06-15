996957ed9785a7e7a34176e99d54cc9f775e0420
Juliann Talkington <perrigoj@bellsouth.net>
2022-06-14 21:22:10 -0500

    solved with digits only

---
+++ can-you-make-the-numbers/answer.js
@@ -0,0 +1,23 @@
+canBuild = (digits, nums) => {
+  const digitCounts = {};
+  for (let i = 0; i < digits; i++) {
+    if(digitsCounts[digits[i]])
+      digitsCounts[digits[i]] = digitsCounts[digits[i]] + 1;
+    else
+      digitsCounts[digits[i]] = 1;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    let strNum = nums[i].toString();
+    for (let j = 0; j < strNum.length; j++) {
+      if (digits[strNum[j]])
+        digits[strNum[j]] = digits[strNum[j]];
+      else
+        return false;
+    }
+  }
+  return true;
+}
+const digits = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+const nums = [];
+
+document.getElementById('ans').textContent = canBuild(digits, nums);
