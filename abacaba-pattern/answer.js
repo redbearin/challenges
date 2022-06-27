cecbd277247aaea7771f09f64561aefa1ebbb2a0
Juliann Talkington <perrigoj@bellsouth.net>
2022-06-27 10:34:11 -0500

    cleaned up code

--- abacaba-pattern/answer.js
+++ abacaba-pattern/answer.js
@@ -1,7 +1,7 @@
 abacabaPattern = n => {
 	if (n < 2) return 'A';
   const prevPattern = abacabaPattern(n - 1);
-  return prevPattern + String.fromCharCode(n + 64) + prevPattern
+  return prevPattern + String.fromCharCode(n + 64) + prevPattern;
 }
 
 const n = 4;
