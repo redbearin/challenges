757e14058faaf0acd7baf5706d18bae4c92518b7
Juliann Talkington <perrigoj@bellsouth.net>
2022-07-15 12:14:15 -0700

    score from game

---
+++ the-primiera/answer.js
@@ -0,0 +1,23 @@
+getPrimieraScore = deck => {
+  let counts = {d: 0, h: 0, s: 0, c:0};
+  let points = {A: 16, 2: 12, 3: 13, 4: 14, 5: 15, 6: 18, 7: 21, J: 10, K: 10, Q: 10 };
+  for (let i = 0; i < deck.length; i++) {
+    if (counts[deck[i][1]] === 0) {
+      counts[deck[i][1]] = points[deck[i][0]];
+    }
+    else {
+      if (points[deck[i][0]] > counts[deck[i][1]])
+        counts[deck[i][1]] = points[deck[i][0]];
+    } 
+  }
+  if (counts.d === 0 || 
+      counts.h === 0 || 
+      counts.s === 0 || 
+      counts.c === 0)
+    return 0;
+  return counts.d + counts.h + counts.s + counts.c;
+}
+
+const deck = ["3d", "6d", "6h", "Qh", "7s", "As", "6c", "Jc"];
+
+document.getElementById('ans').textContent = getPrimieraScore(deck);
