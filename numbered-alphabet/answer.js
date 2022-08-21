a0755d2bd68ce92bfc9af27c5777b5ea9e7a5084
Juliann Talkington <perrigoj@bellsouth.net>
2022-08-20 21:49:40 -0500

    replace letters with numbers

---
+++ numbered-alphabet/answer.js
@@ -0,0 +1,11 @@
+alphNum = str => {
+  let nums = [];
+  for (let i = 0; i < str.length; i++) {
+    nums.push(str.charCodeAt(i) - 65);
+  }
+  return nums.join(' ');
+}
+
+const str = "JAVASCRIPT";
+
+document.getElementById('ans').textContent = alphNum(str);
