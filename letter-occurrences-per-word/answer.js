002c69b20cc88acb3c1d1b8ab2ea915ebe5ffd81
Juliann Talkington <perrigoj@bellsouth.net>
2022-09-20 19:59:07 -0500

    find counts of a specific letter in each word

---
+++ letter-occurrences-per-word/answer.js
@@ -0,0 +1,21 @@
+findOccurrences = (str, char) => {
+  const strArr = str.split(' ');
+  char = char.toLowerCase();
+  const countsByWord = {};
+  let count, word;
+  for (let i = 0; i < strArr.length; i++) {
+    count = 0;
+    word = strArr[i].toLowerCase();
+    for (let j = 0; j < word.length; j++) {
+      if (word[j] === char)
+        count++;
+    }
+    countsByWord[word] = count;
+  }
+  return JSON.stringify(countsByWord);
+}
+
+const str = "Hello World";
+const char = "o";
+
+document.getElementById('ans').textContent = findOccurrences(str, char);
