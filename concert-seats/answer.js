7bdb76668ae8a540ce4db4890f63325d1b003780
Juliann Talkington <perrigoj@bellsouth.net>
2022-09-26 16:55:24 -0500

    can all seats see the stage?

---
+++ concert-seats/answer.js
@@ -0,0 +1,20 @@
+canSeeStage = seats => {
+  //column
+  for (let i = 0; i < seats[0].length; i++) {
+    // row
+    for (let j = 1; j < seats.length; j++) {
+      if (seats[j][i] <= seats[j - 1][i]) {
+	        return false;			
+			}
+    }
+  }
+  return true;
+}
+
+const seats = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+document.getElementById('ans').textContent = canSeeStage(seats);
