8f441794b83ca4c8b7c49b067e05efe501043932
Juliann Talkington <perrigoj@bellsouth.net>
2022-10-15 22:27:16 -0500

    abs different between all digits 1?

---
+++ slidey-numbers/answer.js
@@ -0,0 +1,13 @@
+isSlidey = num => {
+  let numStr = num.toString();
+  for (let i = 1; i < numStr.length; i++) {
+    if (Math.abs(+numStr[i] - +numStr[i - 1]) !== 1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const num = 987654321;
+
+document.getElementById('ans').textContent = isSlidey(num);
