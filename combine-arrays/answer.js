66266170bfad1f35d2724f7d9a071585a94acf28
Juliann Talkington <perrigoj@bellsouth.net>
2022-10-24 20:17:15 -0500

    combine arrays based on index

---
+++ combine-arrays/answer.js
@@ -0,0 +1,24 @@
+combineArrays = (arr1, arr2, arr3) => {
+  const arrs = [arr1, arr2, arr3]
+  const arrSorted = [arr1, arr2, arr3].sort((a, b) => b.length - a.length);
+  const reordered = [];
+  for (let i = 0; i < arrSorted[0].length; i++) {
+    let sub = [];
+    for (let j = 0; j < arrs.length; j++) {
+      if (arrs[j][i] !== undefined) {
+        sub.push(arrs[j][i])
+      }
+      else {
+        sub.push("*");
+      }
+    }
+    reordered.push(sub);
+  }
+  return JSON.stringify(reordered);
+}
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = [7, 8, 9];
+
+document.getElementById('ans').textContent = combineArrays(arr1, arr2, arr3);
