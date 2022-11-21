83b9fcab61151d06e8338e960b36666ce4944c25
Juliann Talkington <perrigoj@bellsouth.net>
2022-11-20 20:46:17 -0600

    add missing vowels to a string

---
+++ c*ns*r*d-str*ngs/answer.js
@@ -0,0 +1,19 @@
+uncensor = str => {
+  let compStr = '';
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '*') {
+      compStr += vowels[count];
+      count++;
+    }
+    else {
+      compStr += str[i];
+    }
+  }
+  return compStr;
+}
+
+const str = "Wh*r* d*d my v*w*ls g*?";
+const vowels = "eeioeo";
+
+document.getElementById('ans').textContent = uncensor(str);
