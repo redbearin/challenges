f075deac2fb50f9a18c2fd835c55529785e7857f
Juliann Talkington <perrigoj@bellsouth.net>
2023-01-01 17:57:21 -0600

    corrected errors

--- longest-abecedarian-word/answer.js
+++ longest-abecedarian-word/answer.js
@@ -1,8 +1,8 @@
 longestAbecedarian = arr => {
-  arrSorted = arr.sort((a,b) => b.length - a.length);
+  const arrSorted = arr.sort((a,b) => b.length - a.length);
   for (let i = 0; i < arr.length; i++) {
     if (arrSorted[i].toLowerCase().split('').sort().join('') === arrSorted[i].toLowerCase())
-      return arr[i];
+      return arrSorted[i];
   }
   return "";
 }
