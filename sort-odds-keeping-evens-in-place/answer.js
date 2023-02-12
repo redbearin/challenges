adf3a40f8ddab3a647e5e5255f7aa4b355d25951
Juliann Talkington <perrigoj@bellsouth.net>
2023-02-11 18:53:50 -0600

    sort odds keep evens in place

---
+++ sort-odds-keeping-evens-in-place/answer.js
@@ -0,0 +1,21 @@
+oddSort = arr => {
+  let odds = [];
+  let oddsIdx = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2) {
+      odds.push(arr[i]);
+    }
+  }
+  odds.sort();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2) {
+      arr[i] = odds[oddsIdx];
+      oddsIdx++;
+    }
+  }
+  return JSON.stringify(arr);
+}
+
+const arr = [7, 5, 2, 3, 1];
+
+document.getElementById('ans').textContent = oddSort(arr);
