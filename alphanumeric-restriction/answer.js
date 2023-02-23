e1877e7d978f09229127818a44fe27d71d104682
Juliann Talkington <perrigoj@bellsouth.net>
2023-02-22 21:25:51 -0600

    all alpha or all numeric?

---
+++ alphanumeric-restriction/answer.js
@@ -0,0 +1,25 @@
+alphanumericRestriction = str => {
+	if (!str.length) {
+		return false;
+	}
+  let ltrCt = 0;
+  let numCt = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (/[^0-9a-zA-Z]/.test(str[i])) {
+      return false;
+    }
+    else {
+      /[1-9]/.test(str[i]) ?
+        numCt++:
+        ltrCt++;
+    }
+    if (ltrCt && numCt) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const str = "H3LL0"
+
+document.getElementById('ans').textContent = alphanumericRestriction(str);
