3fc95a2c9e5307f29cffacaaad71b1daaf4e170a
Juliann Talkington <perrigoj@bellsouth.net>
2023-02-26 13:06:12 -0600

    find missing numbers

---
+++ sum-of-missing-numbers/answer.js
@@ -0,0 +1,13 @@
+sumMissingNumbers = nums => {
+  let sum = 0;
+  for (let i = Math.min(...nums) + 1; i <= Math.max(...nums); i++) {
+    if (nums.includes(i) === false) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+const nums = [4, 3, 8, 1, 2];
+
+document.getElementById('ans').textContent = sumMissingNumbers(nums);
