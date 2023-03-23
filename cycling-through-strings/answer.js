2f576dfaf6ee928bd181293ba09856f4f3645bb9
Juliann Talkington <perrigoj@bellsouth.net>
2023-03-22 21:06:26 -0500

    another code version

--- cycling-through-strings/answer.js
+++ cycling-through-strings/answer.js
@@ -1,6 +1,6 @@
 stringCycling = (str1, str2) => {
-  const times = Math.floor(str2.length/str1.length);
-  return str1.repeat(times) + str1.slice(0, str2.length % str1.length);
+  return str1.repeat(Math.floor(str2.length/str1.length)) + 
+         str1.slice(0, str2.length % str1.length);
 }
 
 const str1 = "abc";
