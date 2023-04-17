738a759661029153e074f236ddc8bf5378304337
Juliann Talkington <perrigoj@bellsouth.net>
2023-04-16 20:08:58 -0500

    simplify code

--- longest-word-in-a-7-segment-display/answer.js
+++ longest-word-in-a-7-segment-display/answer.js
@@ -1,18 +1,15 @@
 longest7SegmentWord = words => {
   let len = 0;
-  let longest;
+  let longest = "";
   for (let i = 0; i < words.length; i++) {
     if (!/[kmvwx]/.test(words[i]) && len < words[i].length) {
       len = words[i].length;
       longest = words[i];
     }
   }
-  if (longest === undefined) {
-    return "";
-  }
   return longest;
 }
 
-const words = ["knighthood", "parental", "fridge", "clingfilm"];
+const words = ["velocity", "mackerel", "woven", "kingsmen"];
 
 document.getElementById('ans').textContent = longest7SegmentWord(words);
