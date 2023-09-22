7401d0c79647e6bc61a455b3a9ef562961dbc972
Juliann Talkington <perrigoj@bellsouth.net>
2023-09-21 22:12:50 -0500

    calc vol of a cone

---
+++ volume-of-a-cone/answer.js
@@ -0,0 +1,8 @@
+coneVolume = (h, r) => {
+  return Math.round(Math.PI * Math.pow(r, 2) * h / 3 * 100) / 100;
+}
+
+const h = 3;
+const r = 2;
+
+document.getElementById('ans').textContent = coneVolume(h, r);
