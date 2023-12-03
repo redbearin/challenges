aafc8ca9f34d5dceab057b7924d6a2b51b1b8121
Juliann Talkington <perrigoj@bellsouth.net>
2023-12-02 20:36:19 -0600

    perform basic math calcs'

---
+++ basic-calculator/answer.js
@@ -0,0 +1,22 @@
+calculator = (num1, op, num2) => {
+  switch (op) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    // …
+    case "/":
+      if (num2 === 0) {
+        return "Can't divide by 0!"
+      }
+      return num1 / num2;
+    default:
+      return num1 * num2;
+  }
+}
+
+const num1 = 2;
+const op = "/";
+const num2 = 2;
+
+document.getElementById('ans').textContent = calculator(num1, op, num2);
