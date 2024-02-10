00c0f24a512d9d54abac6d308f6c317c121e4bca
Juliann Talkington <perrigoj@bellsouth.net>
2024-02-09 23:23:40 -0600

    find all superheroes than end in man

---
+++ superheroes/answer.js
@@ -0,0 +1,16 @@
+superheroes = arr => {
+  const superChars = [];
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i].slice(-5))
+    if (arr[i].slice(-3) === 'man' || arr[i].slice(-3) === 'Man') {
+      if (arr[i].slice(-5) !== 'woman' && arr[i].slice(-5) !== 'Woman') {
+        superChars.push(arr[i]);
+      }
+    }
+  }
+  return JSON.stringify(superChars.sort());
+}
+
+const arr = ["Batman", "Superman", "Spider-man", "Hulk", "Wolverine", "Wonder-Woman"];
+
+document.getElementById('ans').textContent = superheroes(arr);
