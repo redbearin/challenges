0c5f233630fbe6c2141259e57c8567a3e5305da9
Juliann Talkington <perrigoj@bellsouth.net>
2024-03-09 22:04:58 -0600

    shared letter counts

--- count-the-number-of-duplicate-characters2/answer1.js
+++
@@ -1,7 +0,0 @@
-duplicates = str => {
-  return str.length - new Set(str).size;
-}
-
-const str = "The Quick Brown Fox Jumps Over the Lazy Dog";
-
-document.getElementById('ans').textContent = duplicates(str);
