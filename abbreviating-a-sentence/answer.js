09a5e1d547ec659f0d53c035af4922ed6ec66a83
Juliann Talkington <perrigoj@bellsouth.net>
2024-04-11 21:19:26 -0500

    abbrevite a sentence

---
+++ abbreviating-a-sentence/answer.js
@@ -0,0 +1,12 @@
+abbreviate = (str, n = 4) => {
+  const strArr = str.split(' ');
+  let abbr = "";
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i].length >= n) {
+      abbr += strArr[i][0].toUpperCase();
+    }
+  }
+  return abbr;
+}
+
+document.getElementById('ans').textContent = abbreviate("do it yourself", 2);
