b69852801a606eebb7efd0e149d685656cd5713b
Juliann Talkington <perrigoj@bellsouth.net>
2024-05-05 23:12:12 -0500

    add README'

--- free-range/answer.js
+++ free-range/answer.js
@@ -1,28 +1,25 @@
 numbersToRanges = arr => {
   let first = arr[0];
   const ranges = [];
+  if (arr.length === 1) {
+    return JSON.stringify([arr[0].toString()]);
+  }
   for (let i = 1; i < arr.length; i++) {
     if (i === arr.length - 1) {
-      if (first === arr[i]) {
-        ranges.push(arr[i].toString());
-      }
-      else {
+      first === arr[i] ?
+        ranges.push(arr[i].toString()) :
         ranges.push(first.toString() + '-' + arr[i].toString());
-      }
     }
     else if (arr[i] !== arr[i - 1] + 1) {
-      if (first === arr[i - 1]) {
-        ranges.push(first.toString());
-        first = arr[i];
-      }
-      else {
+      first === arr[i - 1]?
+        ranges.push(first.toString()) :
         ranges.push(first.toString() + '-' + arr[i-1].toString());
-        first = arr[i];
-      }
+      first = arr[i];
     }
   }
-  return ranges;
+  return JSON.stringify(ranges);
 }
+
 const arr = [3, 4, 5, 10, 11, 12];
 
 document.getElementById('ans').textContent = numbersToRanges(arr);
