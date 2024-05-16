5ea5f591125128816eab93bb877d752ef9a8675d
Juliann Talkington <perrigoj@bellsouth.net>
2024-05-15 21:12:54 -0500

    check to see if num is automorphic

---
+++ automorphic-numbers/answer.js
@@ -0,0 +1,10 @@
+isAutomorphic = n => {
+  const strNum = Math.pow(n,2).toString();
+  return strNum.slice(-(n.toString().length)) === n.toString() ?
+    true:
+    false;
+}
+
+const n = 25;
+
+document.getElementById('ans').textContent = isAutomorphic(n);
