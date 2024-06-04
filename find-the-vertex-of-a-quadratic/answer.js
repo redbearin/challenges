5bdd55dfac4d9fb1d4902c2257b7b961b5a7bf84
Juliann Talkington <perrigoj@bellsouth.net>
2024-06-04 11:43:21 -0500

    find vertex of quadratic equation

---
+++ find-the-vertex-of-a-quadratic/answer.js
@@ -0,0 +1,13 @@
+findVertex = (a, b, c) => {
+  let k = -(Math.pow(b, 2) - (4*a*c))/(4*a);
+  if (k === -0) {
+    k = 0;
+  }
+  return [-b/(a*2), k];
+}
+
+const a = 1;
+const b = 0;
+const c = 25;
+
+document.getElementById('ans').textContent = findVertex(a, b, c);
