6143c89e02b9c7c891d96b4c5cd923ce7d117ec1
Juliann Talkington <perrigoj@bellsouth.net>
2024-06-07 22:03:29 -0500

    clear fog letters

---
+++ clear-the-fog/answer.js
@@ -0,0 +1,9 @@
+clearFog = str => {
+  const newStr = str.replace(/[fogFOG]/g, "");
+  newStr === str ?
+  
+}
+
+const str = "fogfogFFfoooofftogffreogffesGgfOogfog";
+
+document.getElementById('ans').textContent = clearFog(str);
