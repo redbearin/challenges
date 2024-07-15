7e597f0ffad7ffd409051429bf6e8c9231cf983b
Juliann Talkington <perrigoj@bellsouth.net>
2024-07-14 22:10:38 -0500

    cleanup code

--- how-many-solutions-does-this-quadratic-have/answer.js
+++ how-many-solutions-does-this-quadratic-have/answer.js
@@ -8,19 +8,9 @@ solutions = (a, b, c) => {
   return 0;
 }
 
-//3x^2 - 11x - 4 = 0
-//(3x + 1)(x - 4)
-
 const a = 1;
 const b = 0;
 const c = -1;
 
 document.getElementById('ans').textContent = solutions(a, b, c);
 
-//Test.assertEquals(solutions(1, 0, -1), 2)
-//Test.assertEquals(solutions(1, 0, 0), 1)
-//Test.assertEquals(solutions(1, 0, 1), 0)
-//Test.assertEquals(solutions(200, 420, 800), 0)
-//Test.assertEquals(solutions(200, 420, -800), 2)
-//Test.assertEquals(solutions(1000, 1000, 0), 2)
-//Test.assertEquals(solutions(10000, 400, 4), 1)
