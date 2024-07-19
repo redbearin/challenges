35e5a4bf89b4b4ceaf29b5a47580cc5a90145d23
Juliann Talkington <perrigoj@bellsouth.net>
2024-07-18 21:36:08 -0500

    put letters into correct index order

---
+++ word-builder/answer.js
@@ -0,0 +1,12 @@
+wordBuilder = (ltrs, idx) => {
+  const newArr = Array(ltrs.length).fill('*');
+  for (let i = 0; i < idx.length; i++) {
+    newArr[idx[i]] = ltrs[i];
+  }
+  return newArr.join('');
+}
+
+const ltrs = ["e", "t", "s", "t"];
+const idx = [1, 3, 2, 0];
+
+document.getElementById('ans').textContent = wordBuilder(ltrs, idx);
