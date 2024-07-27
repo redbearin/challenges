47dfe6343e10e1e431a4ee2f6474ecbdaa38048c
Juliann Talkington <perrigoj@bellsouth.net>
2024-07-26 22:35:21 -0500

    remove all the letters from the word

---
+++ remove-the-word/answer.js
@@ -0,0 +1,16 @@
+removeLetters = (arr, str) => {
+  let i = 0;
+  let idx;
+  for (let i = 0; i < str.length; i++) {
+    idx = arr.indexOf(str[i]);
+    if (idx !== -1) {
+      arr.splice(idx, 1);
+    }
+  }
+  return JSON.stringify(arr);
+}
+
+const arr = ["b", "b", "l", "l", "g", "n", "o", "a", "w"];
+const str = "balloon";
+
+document.getElementById('ans').textContent = removeLetters(arr, str);
