936c91a57819944451728da87a44c06fc21db811
Juliann Talkington <perrigoj@bellsouth.net>
2024-08-14 19:58:36 -0500

    count num music styles

---
+++ how-many-unique-styles/answer.js
@@ -0,0 +1,12 @@
+uniqueStyles = arr => {
+  return [... new Set(arr.join(',').split(','))].length;
+}
+
+const arr = ["Dub,Dancehall",
+  "Industrial,Heavy Metal",
+  "Techno,Dubstep",
+  "Synth-pop,Euro-Disco",
+  "Industrial,Techno,Minimal"];
+
+document.getElementById('ans').textContent = 
+uniqueStyles(arr);
