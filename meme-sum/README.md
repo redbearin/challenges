793607cbf0ef534cce8a3cb8948aa2dd298004ed
Juliann Talkington <perrigoj@bellsouth.net>
2024-08-31 21:21:20 -0500

    weird adding'

---
+++ meme-sum/README.md
@@ -0,0 +1,14 @@
+For this challenge, forget how to add two numbers together. The best explanation on what to do for this function is this meme:
+
+Alternative Text
+
+Examples
+memeSum(26, 39) ➞ 515
+// 2+3 = 5, 6+9 = 15
+// 26 + 39 = 515
+
+memeSum(122, 81) ➞ 1103
+// 1+0 = 1, 2+8 = 10, 2+1 = 3
+// 122 + 81 = 1103
+
+memeSum(1222, 30277) ➞ 31499
---
+++ meme-sum/answer.js
@@ -0,0 +1,23 @@
+memeSum = (num1, num2) => {
+  let strNum1 = num1.toString();
+  let strNum2 = num2.toString();
+  const dif = strNum1.length - strNum2.length;
+  let sum = '';
+  if (dif >= 0) {
+    strNum2 = '0'.repeat(dif) + strNum2;
+  }
+  else {
+    strNum1 = '0'.repeat(-dif) + strNum1;
+  }
+  for (let i = 0; i < strNum1.length; i++) {
+    sum += (+strNum1[i] + +strNum2[i]).toString();
+  }
+  return +sum;
+}
+
+const num1 = 121;
+const num2 = 81;
+
+document.getElementById('ans').textContent = memeSum(num1, num2);
+
+3
