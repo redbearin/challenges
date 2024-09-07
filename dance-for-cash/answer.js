491cf8d6f079cc4578f54cb81b5a5a890dc811ce
Juliann Talkington <perrigoj@bellsouth.net>
2024-09-06 19:31:58 -0500

    convent nums to moves

---
+++ dance-for-cash/answer.js
@@ -0,0 +1,15 @@
+const MOVES = ["Shimmy", "Shake", "Pirouette", "Slide", "Box Step", "Headspin", "Dosado", "Pop", "Lock", "Arabesque"];
+
+danceConvert = pin => {
+  const arr = [];
+	for (let i = 0; i < pin.length; i++) {
+    console 
+    arr.push(MOVES[+pin[i] + i])
+  }
+  return JSON.stringify(arr);
+}
+
+const pin = "3856";
+
+document.getElementById('ans').textContent = danceConvert(pin);
+
