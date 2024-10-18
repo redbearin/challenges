cf5f8899fc0e5e2c204746b7653898dca3d33e1d
Juliann Talkington <perrigoj@bellsouth.net>
2024-10-17 22:26:44 -0500

    count instances of ltr in string

---
+++ count-instances-of-character-in-string/answer.js
@@ -0,0 +1,10 @@
+charCount = (ltr, str) => {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === )
+  }
+}
+const ltr = 'a';
+const str = "edabit";
+
+document.getElementById('ans').textContent = charCount(ltr, str);
