a3592887ad286998c1ff347fdcd3761c2ed71b8d
Juliann Talkington <perrigoj@bellsouth.net>
2024-10-17 22:32:53 -0500

    add html

--- count-instances-of-character-in-string/answer.js
+++ count-instances-of-character-in-string/answer.js
@@ -1,9 +1,13 @@
 charCount = (ltr, str) => {
   let count = 0;
   for (let i = 0; i < str.length; i++) {
-    if (str[i] === )
+    if (str[i] === ltr) {
+      count++;
+    }
   }
+  return count;
 }
+
 const ltr = 'a';
 const str = "edabit";
 
