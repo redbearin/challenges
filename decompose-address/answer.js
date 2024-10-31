e87822ad56e3c40f24a3092efa6f6fe7aaa720e8
Juliann Talkington <perrigoj@bellsouth.net>
2024-10-30 20:40:36 -0500

    break the address

---
+++ decompose-address/answer.js
@@ -0,0 +1,14 @@
+decomposeAddress = str => {
+  const strArr = str.split(', ');
+  const first = strArr[0].split(' ');
+  const second = strArr[1].split(' ');
+  const city = [];
+  for (let i = 3; i < first.length; i++) {
+    city.push(first[i]);
+  }
+  return JSON.stringify([first[0], first[1] + ' ' + first[2], city.join(' ').trim(), second[0], second[1]]);
+}
+
+const str = "557 Farmer Rd Corner, MT 59105";
+
+document.getElementById('ans').textContent = decomposeAddress(str);
