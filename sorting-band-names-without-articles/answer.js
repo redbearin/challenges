0108b9b2e1d3b17fa2c5a60c74df72727b870f56
Juliann Talkington <perrigoj@bellsouth.net>
2024-11-02 23:14:16 -0500

    sorting without articles

---
+++ sorting-band-names-without-articles/answer.js
@@ -0,0 +1,27 @@
+bandNamesSort = arr => {
+  const newArr = [];
+  let sub;
+  let newSub;
+  for (let i = 0; i < arr.length; i++) {
+    sub = arr[i].split(' ');
+    for (let j = 0; j < sub.length; j++) {
+      if (sub[j] !== "An" && 
+          sub[j] !== "A" && 
+          sub[j] !== "The") {
+        newSub = sub.slice(j);
+        break;
+      }
+    }
+    newArr.push([newSub[0].toLowerCase(), arr[i]]);
+  }
+  newArr.sort();
+  let finalArr = [];
+  for (let i = 0; i < newArr.length; i++) {
+    finalArr.push(newArr[i][1]);
+  }
+  return JSON.stringify(finalArr);
+}
+
+const arr = ["The New Yardbirds", "The Beatles", "The Square Roots", "On A Friday", "An Irony"];
+
+document.getElementById('ans').textContent = bandNamesSort(arr);
