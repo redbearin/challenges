d2d1a196e606601a2a84645dcc0726c48b365b72
Juliann Talkington <perrigoj@bellsouth.net>
2025-01-19 19:55:00 -0600

    provide listing of who is online

---
+++ chat-room-status/answer.js
@@ -0,0 +1,22 @@
+chatroomStatus = arr => {
+  let front;
+  if(!arr.length) {
+    front = "no one ";
+    console.log(front)
+  }
+  else if (arr.length === 1)  {
+    front = arr[0];
+  }
+  else if (arr.length === 2) {
+
+    front = arr[0] + " and " + arr[1];
+  }
+  else {
+    front = arr[0] + ",  " + arr[1] + " and " + (arr.length - 2) + " more";
+  }
+  return front + " online";
+}
+
+const arr = [];
+
+document.getElementById('ans').textContent = chatroomStatus(arr);
