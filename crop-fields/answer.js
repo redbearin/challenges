0e510d9af6db4887454b8d62e89d06a6c068f9dd
Juliann Talkington <perrigoj@bellsouth.net>
2025-01-25 16:46:52 -0600

    all end cases working

--- crop-fields/answer.js
+++ crop-fields/answer.js
@@ -1,22 +1,38 @@
-// all but cases when there are less than 3 columns and less than 3 rows
-
 cropHydrated = arr => {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr[i].length - 1; j++) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
       // top left
       if (i === 0 && j === 0) {
         if (arr[i][j] === 'c') {
-          if (arr[i+1][j] === 'c' &&
-            arr[i+1][j+1] === 'c' &&
-            arr[i][j+1] === 'c') {
+          // one row
+          if (arr.length === 1) {
+            // one unit
+            if (arr[i].length === 1) {
+              return false;
+            }
+            // more than one unit
+            if (arr[i][j+1] === 'c') {
+              return false;
+            }
+          }
+          // multiple rows
+          else if (arr[i+1][j] === 'c' &&
+                arr[i+1][j+1] === 'c' &&
+                arr[i][j+1] === 'c') {
             return false;
-          }   
+          }
         }
       }
       // top right
       else if (i === 0 && j === arr[i].length - 1) {
         if (arr[i][j] === 'c') {
-          if (arr[i+1][j] === 'c' &&
+          // one row
+          if (arr.length === 1 && arr[i][j-1] === 'c') {
+            return false;
+          }
+          // multiple rows
+          if (arr.length !== 1 &&
+            arr[i+1][j] === 'c' &&
             arr[i+1][j-1] === 'c' &&
             arr[i][j-1] === 'c') {
             return false;
@@ -26,6 +42,11 @@ cropHydrated = arr => {
       // bottom left
       else if (i === arr.length - 1 && j === 0) {
         if (arr[i][j] === 'c') {
+          // one row
+          if (arr.length === 1 && arr[i][j+1] === 'c') {
+            return false;
+          }
+          //multiple rows
           if (arr[i-1][j] === 'c' &&
             arr[i-1][j+1] === 'c' &&
             arr[i][j+1] === 'c') {
@@ -36,6 +57,11 @@ cropHydrated = arr => {
       // bottom right
       else if (i === arr.length - 1 && j === arr[i].length - 1) {
         if (arr[i][j] === 'c') {
+          // one row
+          if (arr.length === 1 && arr[i][j-1] === 'c') {
+            return false;
+          }
+          //multiple rows
           if (arr[i-1][j] === 'c' &&
             arr[i-1][j-1] === 'c' &&
             arr[i][j-1] === 'c') {
@@ -46,7 +72,16 @@ cropHydrated = arr => {
       // top row in the center
       else if (i === 0) {
         if (arr[i][j] === 'c') {
-          if (arr[i+1][j] === 'c' &&
+          // one row
+          if (arr.length === 1 && 
+            arr[i][j-1] === 'c' &&
+            arr[i][j+1] === 'c') {
+            console.log('top row center one row')
+            return false;
+          }
+          // multiple rows
+          if (arr.length !== 1 &&
+            arr[i+1][j] === 'c' &&
             arr[i+1][j-1] === 'c' &&
             arr[i+1][j+1] === 'c' &&
             arr[i][j-1] === 'c' &&
@@ -56,8 +91,10 @@ cropHydrated = arr => {
         }
       }
       //bottom row center
-      else if (i === arr.length - 1) {
+      else if (i === arr.length - 1 && arr.length !== 1) {  
         if (arr[i][j] === 'c') {
+          // one row already handled above
+          // multiple rows
           if (arr[i-1][j] === 'c' &&
             arr[i-1][j-1] === 'c' &&
             arr[i-1][j+1] === 'c' &&
@@ -70,6 +107,13 @@ cropHydrated = arr => {
       // left column center
       else if (j === 0) {
         if (arr[i][j] === 'c') {
+          // one column
+          if (arr[i].length === 1 &&
+            arr[i-1][j] === 'c' &&
+            arr[i+1][j] === 'c') {
+            return false;
+          }
+          //multiple columns
           if (arr[i-1][j] === 'c' &&
             arr[i+1][j] === 'c' &&
             arr[i-1][j+1] === 'c' &&
@@ -80,8 +124,11 @@ cropHydrated = arr => {
         }
       }
       // right column center
-      else if (j === arr[i].length -1) {
+      else if (j === arr[i].length -1 && 
+               arr[i].length !== 1) {
         if (arr[i][j] === 'c') {
+          //one column already handled above
+          //multiple columns
           if (arr[i-1][j] === 'c' &&
             arr[i+1][j] === 'c' &&
             arr[i-1][j-1] === 'c' &&
@@ -112,10 +159,7 @@ cropHydrated = arr => {
 }
 
 const arr = [
-  [ "c", "w", "w", "w", "c" ],
-  [ "w", "c", "c", "c", "c" ],
-  [ "c", "c", "c", "c", "c" ],
-  [ "w", "c", "c", "w", "c" ]
-];
+    [ "c", "w", "w", "c", "c", "w", "c" ]
+  ];
 
 document.getElementById('ans').textContent = cropHydrated(arr);
