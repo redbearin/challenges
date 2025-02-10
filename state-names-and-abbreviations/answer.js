4ffc7c17a4797c64805544b44f0bd1bb6d894915
Juliann Talkington <perrigoj@bellsouth.net>
2025-02-10 09:52:28 -0600

    sort by name or abbreviation

---
+++ state-names-and-abbreviations/answer.js
@@ -0,0 +1,11 @@
+filterStateNames = (arr, typ) => {
+  const newArr = [];
+  return typ === 'abb'?
+  JSON.stringify(arr.filter(ele => ele.charCodeAt(1) < 91)) :
+  JSON.stringify(arr.filter(ele => ele.charCodeAt(1) > 96)) ;
+}
+
+const arr = ["Arizona", "CA", "NY", "Nevada"];
+const typ = "full";
+
+document.getElementById('ans').textContent = filterStateNames(arr, typ);
