d7005ca6c7386a4db34ec3661ade9a8386644355
Juliann Talkington <perrigoj@bellsouth.net>
2025-06-10 21:53:00 -0500

    sort positives and keep negs

---
+++ sort-positives-keep-negatives/answer.js
@@ -0,0 +1,19 @@
+posNegSort = arr => {
+  let posNum = [];
+  for (let i = 0; i< arr.length; i++) {
+    if (arr[i] > 0) {
+      posNum.push(arr[i]);
+    }
+  }
+  posNum.sort();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      arr[i] = posNum.shift();
+    }
+  }
+  return JSON.stringify(arr);
+}
+
+const arr = [6, 3, -2, 5, -8, 2, -2];
+
+document.getElementById('ans').textContent = posNegSort(arr);
