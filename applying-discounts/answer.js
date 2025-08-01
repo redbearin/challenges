2ded2fe665a277d043a2d6a425e7430969e526b4
Juliann Talkington <perrigoj@bellsouth.net>
2025-08-01 15:12:16 -0500

    discounted amount

---
+++ applying-discounts/answer.js
@@ -0,0 +1,9 @@
+getDiscounts = (arr, per) => {
+  const dec = per.slice(0, -1)/100;
+  return JSON.stringify(arr.map((ele) => ele * dec));
+}
+
+const arr = [2, 4, 6, 11];
+const per = "50%";
+
+document.getElementById('ans').textContent = getDiscounts(arr, per);
