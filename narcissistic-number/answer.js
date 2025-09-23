b6ebf66ebb698583b799a9350f25f89630062839
Juliann Talkington <perrigoj@bellsouth.net>
2025-09-23 18:51:03 -0500

    is the number narcissistic?

---
+++ narcissistic-number/answer.js
@@ -0,0 +1,8 @@
+isNarcissistic = num => {
+  const len = num.toString().length;
+  return num.toString().split('').map(Number).reduce((acc, ele) => Math.pow(ele, len) + acc, 0) === num;
+}
+
+const num = 153;
+
+document.getElementById('ans').textContent = isNarcissistic(num);
