de6dd6ab52c0302ca918fdd40221dcb53d47341e
Juliann Talkington <perrigoj@bellsouth.net>
2025-10-27 21:19:06 -0500

    trace the route

---
+++ route-tracer/answer.js
@@ -0,0 +1,39 @@
+routeTracer = (grid, path) => {
+  const grid1 = [...grid];
+  let str = '';
+  let current;
+  for (let i = 0; i < grid.length; i++) {
+    if (grid[i].includes('@')) {
+      current = [i, grid[i].indexOf('@')];
+    }
+  }
+  for (let i = 0; i < path.length; i++) {
+    if (path[i] === 'l') {
+      current = [current[0], current[1] - 1];
+    }
+    if (path[i] === 'r') {
+      current = [current[0], current[1] + 1];
+    }
+    if (path[i] === 'u') {
+      current = [current[0] - 1, current[1]];
+    }
+    if (path[i] === 'd') {
+      current = [current[0] + 1, current[1]];
+    }
+    if (grid[current[0]][current[1]] !== "-") {
+      str += grid[current[0]][current[1]];
+    }
+    grid1[current[0]][current[1]] = 'x';
+  }
+  return JSON.stringify({route: grid1, items: str});
+}
+
+const grid = [
+  ["-", "#", "-", "-"],
+  ["$", "#", "@", "&"],
+  ["-", "#", "$", "&"],
+  ["$", "-", "&", "-"]
+];
+const path = "lddr";
+
+document.getElementById('ans').textContent = routeTracer(grid, path);
