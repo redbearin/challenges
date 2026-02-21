24e267e67bad0e9ade2692186cd16d46c577699c
Juliann Talkington <perrigoj@bellsouth.net>
2026-02-21 10:05:42 -0600

    clean-up

--- minimal-IV-if-else-if-else-inferno/answer.js
+++ minimal-IV-if-else-if-else-inferno/answer.js
@@ -1,9 +1,9 @@
 compare_to_100 = x => {
   if (x === 0) return true;
   if (x < 0) return "negative";
-  return "positive"
+  return "positive";
 }
 
-const x = 2
+const x = 2;
 
 document.getElementById('ans').textContent = compare_to_100(x);
