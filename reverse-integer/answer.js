0e067d7e55da756493aebecb072a338d5f5be045
Juliann Talkington <perrigoj@bellsouth.net>
2026-05-22 20:52:36 -0500

    reverse a number

---
+++ reverse-integer/answer.js
@@ -0,0 +1,9 @@
+reverseInt = num => {
+  const strNum = num.toString().split('').reverse().join('');
+  if (num < 0) {
+    return - strNum.slice(0, -1);
+  }
+}
+const num = -321
+
+document.getElementById('ans').textContent = reverseInt(num);
