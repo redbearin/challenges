9da5a30e19a3b6afa6b4a5bdeee86de9de6d768e
Juliann Talkington <perrigoj@bellsouth.net>
2026-05-30 20:38:02 -0500

    easier to read option

--- length-of-last-word/answer.js
+++ length-of-last-word/answer.js
@@ -1,5 +1,5 @@
 lenLastWord = str => str.replace(/\s+/g, ' ').split(' ').sort((a, b) => b.length - a.length)[0].length;
 
-const str = "   fly me   to   the moon  ";
+const str = "fly me to the moon ";
 
 document.getElementById('ans').textContent = lenLastWord(str);
