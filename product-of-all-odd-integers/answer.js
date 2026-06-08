594cf13c27f3a7674726e00fdf0648056d8db6ee
Juliann Talkington <perrigoj@bellsouth.net>
2026-06-08 18:25:05 -0500

    product of all odd integers

---
+++ product-of-all-odd-integers/answer.js
@@ -0,0 +1,10 @@
+oddProduct = arr => arr.reduce((acc, ele) => {
+  if (ele % 2) {
+    return acc * ele;
+  }
+  return acc;
+}, 1);
+
+const arr = [3, 4, 1, 1, 5];
+
+document.getElementById('ans').textContent = oddProduct(arr);
