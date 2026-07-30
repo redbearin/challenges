b0b49b27594ea08fe614cc0d5d314c77c2cd5b7f
Juliann Talkington <perrigoj@bellsouth.net>
2026-07-29 22:11:01 -0500

    find the factors

---
+++ factors-of-a-given-number/answer.js
@@ -0,0 +1,13 @@
+findFactors = num => {
+  const factors = [];
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) {
+      factors.push(i);
+    }
+  }
+  return JSON.stringify(factors);
+}
+
+const num = 9;
+
+document.getElementById('ans').textContent = findFactors(num);
