03684d22a66fef387eb6ffc2d048c6ad4cc37621
Juliann Talkington <perrigoj@bellsouth.net>
2026-08-04 18:23:26 -0500

    meets parseltongue criteria

---
+++ parseltongue/answer.js
@@ -0,0 +1,26 @@
+isParselTongue = str => {
+  str = str.toLowerCase();
+  const strArr = str.split(' ');
+  let flag = false;
+  for (let i = 0; i < strArr.length; i++) { 
+    for (let j = 0; j < strArr[i].length; j++) {
+      if (strArr[i][j] === 's' && 
+          strArr[i][j - 1] === 's' || 
+          strArr[i][j + 1] === 's') {
+          break;
+      }
+      if (strArr[i][j] === 's') {
+        flag = true;
+      }
+    }
+    if (flag) {
+      return false;
+    }
+    flag = false;
+  }
+  return true;
+}
+
+const str = "She ssselects to eat that apple. ";
+
+document.getElementById('ans').textContent = isParselTongue(str);
