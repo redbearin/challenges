b81fdfbb4ac4417b159b5c0451c6cf8684089643
Juliann Talkington <perrigoj@bellsouth.net>
2026-09-01 21:00:01 -0500

    find the first number that is duplicated

---
+++ first-duplicate/answer.js
@@ -0,0 +1,17 @@
+const firstDuplicate = (arr) => {
+  const seen = new Set();
+  for (const num of arr) {
+    if (seen.has(num)) {
+      return num;
+    }
+    else {
+      seen.add(num);
+    }
+    console.log(seen)
+  }
+  return "no repeats";
+};
+
+const arr = [2, 1, 3, 5, 3, 2];
+
+document.getElementById('ans').textContent = firstDuplicate(arr);
